a5aa1a131f145eb4eabb521d7a242109b38d9c09
Isak Bayat <Isakbayat05@gmail.com>
2026-09-09 16:22:14 +0200

    Add applications GET endpoint

---
+++ JobTrack.Api/Controllers/ApplicationsController.cs
@@ -0,0 +1,37 @@
+﻿using JobTrack.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobTrack.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationsController : ControllerBase
+    {
+        private static readonly List<JobApplication> applications = new List<JobApplication>
+        {
+            new JobApplication
+            {
+                Id = 1,
+                Company = "Telenor",
+                Position = "System Developer Intern",
+                AppliedDate = new DateTime(2026, 9, 9),
+                Status = "Applied"
+            },
+
+            new JobApplication
+            {
+                Id = 2,
+                Company = "SEB",
+                Position = ".NET Developer Intern",
+                AppliedDate = new DateTime(2026, 9, 8),
+                Status = "Interview"
+            }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<JobApplication>> GetAll()
+        {
+            return Ok(applications);
+        }
+    }
+}
